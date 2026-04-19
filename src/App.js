17cab865ed2cbebbf23f2269ad8cf50bc8152444
renxss <singhrenee80@gmail.com>
2026-04-19 11:50:21 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./Shecodeslogo.png.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="SheCodes logo" />
+        <span className="App-title"></span>
+      </header>
+    </div>
+  );
+}
