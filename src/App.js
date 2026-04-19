a14e12b5116d970c9e85a0e41f1b619c91994e08
renxss <singhrenee80@gmail.com>
2026-04-19 19:00:52 +1000

    updated search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./Shecodeslogo.png.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="SheCodes logo" />
-        <span className="App-title"></span>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="SheCodes logo" />
+
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Renee Singh</small>
+        </footer>
+      </div>
     </div>
   );
 }
