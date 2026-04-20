ea2f6f81f317e8552e36b67784b8776be47d5e83
renxss <singhrenee80@gmail.com>
2026-04-21 01:14:24 +1000

    features

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,33 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  let [photos, setPhotos] = useState(null);
 
   function handleDictionResponse(response) {
     setResults(response.data[0]);
-  }
-
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleDictionResponse);
-
-    let pexelsApiKey =
-      "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -47,7 +35,7 @@ export default function Dictionary(props) {
   if (loaded) {
     return (
       <div className="Dictionary">
-        <section>
+        <div className="search-area">
           <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input
@@ -59,9 +47,8 @@ export default function Dictionary(props) {
           <div className="hint">
             suggested words: sunset, wine, yoga, plant...
           </div>
-        </section>
+        </div>
         <Results results={results} />
-        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <section>
+        <div>
           <h2>{props.results.word}</h2>
           {props.results.phonetics.map(function (phonetic, index) {
             return (
@@ -16,12 +16,12 @@ export default function Results(props) {
               </div>
             );
           })}
-        </section>
+        </div>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <section key={index}>
+            <div key={index}>
               <Meaning meaning={meaning} />
-            </section>
+            </div>
           );
         })}
       </div>
