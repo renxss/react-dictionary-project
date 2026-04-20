41e4c9366b05708e21232495ed3abd4c402f0a9c
renxss <singhrenee80@gmail.com>
2026-04-21 01:28:59 +1000

    additional graphics

--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <div>
+        <section>
           <h2>{props.results.word}</h2>
           {props.results.phonetics.map(function (phonetic, index) {
             return (
@@ -16,12 +16,12 @@ export default function Results(props) {
               </div>
             );
           })}
-        </div>
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <section key={index}>
               <Meaning meaning={meaning} />
-            </div>
+            </section>
           );
         })}
       </div>
